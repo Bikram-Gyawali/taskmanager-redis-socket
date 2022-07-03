a03d0ddd57ca15788cf9a58ae1cf979370c6881e
Bikram-Gyawali <bikramgyawali57@gmail.com>
2022-07-03 23:16:37 +0545

    header section

---
+++ frontend/components/header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+function Header() {
+  return (
+    <div className="header-main">
+      <div className="logo">Superteam</div>
+      <div className="profile">
+        <h1>--0=0--|</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
